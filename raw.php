de25ae5de6fda7cd0854bc08e48deecc357c8c50
Martin Mirbauer <martinm@cgg.mff.cuni.cz>
2019-09-16 13:50:53 +0200

    added Carmelita

--- raw.php
+++ raw.php
@@ -214,6 +214,42 @@ function hamu() {
     return $menuClean;
 }
 
+function carmelita() {
+    global $dayOfWeek;
+    
+    $pageRaw = file_get_contents("http://www.restauracecarmelita.cz/poledni-nabidka-1/");
+	$dom = new DomDocument();
+    // The web is missing encoding header, so appends it manually.
+    @$dom->loadHTML('<?xml encoding="utf-8" ? >' .  $pageRaw);
+	$finder = new DomXPath($dom);
+	$nodes = $finder->query("//div[@id='content']");
+    
+
+	$pageRaw = strip_tags($dom->saveHTML($nodes[0]));
+	$pageRaw = str_replace(array("\n", "\r"), "<br>", $pageRaw);
+
+    $separators = array("Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "");
+    $sepA = $separators[$dayOfWeek];
+    $sepB = $separators[$dayOfWeek+1];
+    preg_match('/.*' . $sepA . '(.*)' . $sepB . '.*/', $pageRaw, $menuDirty);
+    
+    $menuClean = $menuDirty[1];
+    
+    $menuClean = preg_replace('/\d+\.\d+\./', "", $menuClean);
+
+    // Remove order ("1.", "2.", ...)
+    $menuClean = preg_replace('/\d\. /', "", $menuClean);
+    // Clean prices
+    $menuClean = preg_replace('/\d+,- Kč<br>/', "", $menuClean);
+    $menuClean = preg_replace('/\d+g/', "", $menuClean);
+    // Collapse newlines
+    $menuClean = preg_replace('/<br>(<br>|\s)+/', "<br>", $menuClean);
+    // Drop leading whitespace
+    $menuClean = preg_replace('/^ *<br>/', "", $menuClean);
+
+    return $menuClean;
+}
+
 function menza_prava() {
     global $dayOfWeek;
     // This may get broken soon. I don't know how to choose the lawyer's menza explicitly 
@@ -264,6 +300,11 @@ $places = array(
         'func' => 'menza_prava',
         'name' => 'Právnická fakulta',
         'href' => 'https://kamweb.ruk.cuni.cz/webkredit/',
+    ),
+    array(
+        'func' => 'carmelita',
+        'name' => 'Carmelita',
+        'href' => 'http://www.restauracecarmelita.cz/',
     ),
 );
 $response = array();
