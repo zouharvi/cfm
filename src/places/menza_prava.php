73bbbd2f0cd854f24644aa73539b59087bf5b572
Vilem Zouhar <vilem.zouhar@gmail.com>
2019-10-07 12:23:32 +0200

    fix menza

--- src/places/menza_prava.php
+++ src/places/menza_prava.php
@@ -6,7 +6,7 @@ function menza_prava() {
     $dom = new DomDocument();
     // The web is missing encoding header, so appends it manually.
     @$dom->loadHTML('<?xml encoding="utf-8" ? >' .  $pageRaw);
-    $alternatives = array(5, 7, 9, 11, 13, 15, 17); 
+    $alternatives = array(5, 7, 9, 11, 13, 15, 17, 19, 21); 
     $menu = array();
     foreach($alternatives as $alternative) {
         $menuEl = $dom->getElementById('Jidelnicek1_AlternativaTxt' . $alternative);
